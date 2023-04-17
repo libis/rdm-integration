0bf5eebc335ede17ac06880cd7f0b078aad942be
Eryk Kulikowski <eryk.kulikowski@kuleuven.be>
2023-04-17 10:34:32 +0200

    drop_permissions set to true

--- image/app/core/oath.go
+++ image/app/core/oath.go
@@ -173,7 +173,7 @@ func encode(req OauthTokenRequest) *bytes.Buffer {
 
 func doExchange(ctx context.Context, in OauthTokenResponse, url string) (OauthTokenResponse, error) {
 	res := in
-	req := ExchangeRequest{false, in.AccessToken}
+	req := ExchangeRequest{true, in.AccessToken}
 	data, _ := json.Marshal(req)
 	body := bytes.NewBuffer(data)
 	request, _ := http.NewRequestWithContext(ctx, "POST", url, body)
