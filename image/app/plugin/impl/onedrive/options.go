3956a92e85f829d1a41b7a87748d3ee6840c832f
Eryk Kulikowski <eryk.kulikowski@kuleuven.be>
2023-10-19 08:58:52 +0200

    better error handling in onedrive plugin

--- image/app/plugin/impl/onedrive/options.go
+++ image/app/plugin/impl/onedrive/options.go
@@ -14,7 +14,17 @@ func Options(ctx context.Context, params types.OptionsRequest) ([]types.SelectIt
 	if params.Url == "" || params.Token == "" {
 		return nil, fmt.Errorf("streams: missing parameters: expected url, token, got: %+v", params)
 	}
-	drives, err := getResponse(ctx, params.Url+"/me/drives", params.Token)
+	drives, err := getResponse(ctx, params.Url+"/me/followedSites", params.Token)
+	/*
+		drives, err = getResponse(ctx, params.Url+"/sites/kuleuven.sharepoint.com:/sites/LiriasRDM-RDR-BIBandLIBIS:/sites", params.Token)
+		fmt.Printf("/sites/kuleuven.sharepoint.com:/sites/LiriasRDM-RDR-BIBandLIBIS:/sites - %+v - %v\n\n", drives, err)
+		drives, err = getResponse(ctx, params.Url+"/sites/kuleuven.sharepoint.com:/sites/LiriasRDM-RDR-BIBandLIBIS:/Shared Documents", params.Token)
+		fmt.Printf("/sites/kuleuven.sharepoint.com:/sites/LiriasRDM-RDR-BIBandLIBIS:/Shared Documents - %+v - %v\n\n", drives, err)
+		drives, err = getResponse(ctx, params.Url+"/sites/kuleuven.sharepoint.com:/sites/LiriasRDM-RDR-BIBandLIBIS:/Documents", params.Token)
+		fmt.Printf("/sites/kuleuven.sharepoint.com:/sites/LiriasRDM-RDR-BIBandLIBIS:/Documents - %+v - %v\n\n", drives, err)
+
+		https://kuleuven.sharepoint.com/:f:/r/sites/LiriasRDM-RDR-BIBandLIBIS/Shared%20Documents/Communicatie%20%26%20NewsItems?csf=1&web=1&e=dEtblA
+	*/
 	if err != nil {
 		return nil, err
 	}
