18892fa6ceed9e340a88d94ecd61077040e4407e
Eryk Kulikowski <eryk.kulikowski@kuleuven.be>
2023-10-25 16:08:24 +0200

    removed comment

--- image/app/plugin/impl/onedrive/options.go
+++ image/app/plugin/impl/onedrive/options.go
@@ -15,17 +15,6 @@ func Options(ctx context.Context, params types.OptionsRequest) ([]types.SelectIt
 		return nil, fmt.Errorf("streams: missing parameters: expected url, token, got: %+v", params)
 	}
 	drives, err := getResponse(ctx, params.Url+"/me/drives", params.Token)
-	/*
-	    drives, err := getResponse(ctx, params.Url+"/me/followedSites", params.Token)//500 - accessDenied: This operation is not supported with the provided scopes
-		drives, err = getResponse(ctx, params.Url+"/sites/kuleuven.sharepoint.com:/sites/LiriasRDM-RDR-BIBandLIBIS:/sites", params.Token)
-		fmt.Printf("/sites/kuleuven.sharepoint.com:/sites/LiriasRDM-RDR-BIBandLIBIS:/sites - %+v - %v\n\n", drives, err)
-		drives, err = getResponse(ctx, params.Url+"/sites/kuleuven.sharepoint.com:/sites/LiriasRDM-RDR-BIBandLIBIS:/Shared Documents", params.Token)
-		fmt.Printf("/sites/kuleuven.sharepoint.com:/sites/LiriasRDM-RDR-BIBandLIBIS:/Shared Documents - %+v - %v\n\n", drives, err)
-		drives, err = getResponse(ctx, params.Url+"/sites/kuleuven.sharepoint.com:/sites/LiriasRDM-RDR-BIBandLIBIS:/Documents", params.Token)
-		fmt.Printf("/sites/kuleuven.sharepoint.com:/sites/LiriasRDM-RDR-BIBandLIBIS:/Documents - %+v - %v\n\n", drives, err)
-
-		https://kuleuven.sharepoint.com/:f:/r/sites/LiriasRDM-RDR-BIBandLIBIS/Shared%20Documents/Communicatie%20%26%20NewsItems?csf=1&web=1&e=dEtblA
-	*/
 	if err != nil {
 		return nil, err
 	}
