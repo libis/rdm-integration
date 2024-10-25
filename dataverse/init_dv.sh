4b5a04926449f127d69e5d89c6543735bcf457bb
Eryk Kullikowski <eryk.kulikowski@gmail.com>
2024-10-25 17:05:13 +0200

    bearer token implementation

--- dataverse/init_dv.sh
+++ dataverse/init_dv.sh
@@ -1,6 +1,5 @@
 #!/usr/bin/env bash
 
-# Check if already initialized
-[[ -f "/dv/init/initialized" ]] && exit 0
-
-bash /opt/payara/scripts/setup.sh &
+if [ ! -f /dv/initialized ]; then
+    bash /opt/payara/scripts/setup.sh &
+fi
