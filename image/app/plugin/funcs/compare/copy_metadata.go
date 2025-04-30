55ebb27e0c4675b54ca8a0f4caa2c6cdd643d02b
Eryk Kulikowski <eryk.kulikowski@kuleuven.be>
2025-04-30 09:54:52 +0200

    refactoring of metadata copy

--- image/app/plugin/funcs/compare/copy_metadata.go
+++ image/app/plugin/funcs/compare/copy_metadata.go
@@ -15,6 +15,8 @@ import (
 	"github.com/libis/rdm-dataverse-go-api/api"
 )
 
+type Metadata map[string]interface{}
+
 func copyMetaData(compareRequest types.CompareRequest, user string) error {
 	ctx, cancel := context.WithTimeout(context.Background(), time.Minute)
 	defer cancel()
@@ -25,28 +27,32 @@ func copyMetaData(compareRequest types.CompareRequest, user string) error {
 	return putMetadata(ctx, compareRequest, user, data)
 }
 
-func getMetadata(ctx context.Context, compareRequest types.CompareRequest, user string) ([]byte, error) {
+func getMetadata(ctx context.Context, compareRequest types.CompareRequest, user string) (Metadata, error) {
 	from := "/api/v1/datasets/:persistentId/versions/:latest?persistentId=" + compareRequest.RepoName
 	fromClient := dv.NewClient(compareRequest.PluginId, compareRequest.Url, user, compareRequest.Token)
 	fromRequest := fromClient.NewRequest(from, "GET", nil, nil)
-	md := map[string]interface{}{}
-	err := api.Do(ctx, fromRequest, &md)
+	res := map[string]interface{}{}
+	err := api.Do(ctx, fromRequest, &res)
 	if err != nil {
 		return nil, err
 	}
-	if md["status"] != "OK" {
-		return nil, fmt.Errorf("metadata copy failed: %v", md["message"])
+	if res["status"] != "OK" {
+		return nil, fmt.Errorf("metadata copy failed: %v", res["message"])
 	}
-	metadataBlocks := map[string]interface{}{}
-	metadataBlocks["metadataBlocks"] = md["data"].(map[string]interface{})["metadataBlocks"]
-	return json.Marshal(metadataBlocks)
+	metadataBlocks := Metadata{}
+	metadataBlocks["metadataBlocks"] = res["data"].(map[string]interface{})["metadataBlocks"]
+	return metadataBlocks, nil
 }
 
-func putMetadata(ctx context.Context, compareRequest types.CompareRequest, user string, data []byte) error {
+func putMetadata(ctx context.Context, compareRequest types.CompareRequest, user string, data Metadata) error {
 	to := "/api/v1/datasets/:persistentId/versions/:draft?persistentId=" + compareRequest.PersistentId
-	toReq := dataverse.GetRequest(to, "PUT", user, compareRequest.DataverseKey, bytes.NewBuffer(data), api.JsonContentHeader())
+	dataBytes, err := json.Marshal(data)
+	if err != nil {
+		return err
+	}
+	toReq := dataverse.GetRequest(to, "PUT", user, compareRequest.DataverseKey, bytes.NewBuffer(dataBytes), api.JsonContentHeader())
 	res := map[string]interface{}{}
-	err := api.Do(ctx, toReq, &res)
+	err = api.Do(ctx, toReq, &res)
 	if err != nil {
 		return err
 	}
