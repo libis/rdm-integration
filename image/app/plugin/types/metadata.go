f418c6357ae666e1034e044a87cc28c72463b84b
Eryk Kullikowski <eryk.kulikowski@gmail.com>
2025-05-03 16:15:53 +0200

    added compare result to the get metadata request

--- image/app/plugin/types/metadata.go
+++ image/app/plugin/types/metadata.go
@@ -1,5 +1,7 @@
 package types
 
+import "integration/app/tree"
+
 const NewDataset = "New Dataset"
 
 type Metadata map[string]interface{}
@@ -34,11 +36,21 @@ type GrantNumber struct {
 }
 
 type GetMetadataRequest struct {
-	PluginId string `json:"pluginId"`
-	Plugin   string `json:"plugin"`
-	RepoName string `json:"repoName"`
-	Url      string `json:"url"`
-	Option   string `json:"option"`
-	User     string `json:"user"`
-	Token    string `json:"token"`
+	PluginId      string          `json:"pluginId"`
+	Plugin        string          `json:"plugin"`
+	RepoName      string          `json:"repoName"`
+	Url           string          `json:"url"`
+	Option        string          `json:"option"`
+	User          string          `json:"user"`
+	Token         string          `json:"token"`
+	CompareResult CompareResponse `json:"compareResult"`
+}
+
+type CompareResponse struct {
+	Id          string      `json:"id"`
+	Status      int         `json:"status"`
+	Data        []tree.Node `json:"data"`
+	Url         string      `json:"url"`
+	MaxFileSize int64       `json:"maxFileSize,omitempty"`
+	Rejected    []string    `json:"rejected,omitempty"`
 }
