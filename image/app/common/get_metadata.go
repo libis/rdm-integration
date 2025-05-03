6de3e930930ec17d8988cdb3501a8ee595970a33
Eryk Kullikowski <eryk.kulikowski@gmail.com>
2025-05-03 15:55:27 +0200

    metadata template

--- image/app/common/get_metadata.go
+++ image/app/common/get_metadata.go
@@ -3,14 +3,18 @@
 package common
 
 import (
+	"bufio"
+	"bytes"
 	"context"
 	"encoding/json"
 	"fmt"
 	"integration/app/core"
+	"integration/app/dataverse"
 	dv "integration/app/plugin/impl/dataverse"
 	"integration/app/plugin/types"
 	"io"
 	"net/http"
+	"text/template"
 
 	"github.com/libis/rdm-dataverse-go-api/api"
 )
@@ -49,21 +53,220 @@ func GetMetadata(w http.ResponseWriter, r *http.Request) {
 }
 
 func getMetadata(ctx context.Context, getMetadataRequest types.GetMetadataRequest, user string) (types.Metadata, error) {
-	if getMetadataRequest.Plugin != "dataverse" {
-		return nil, nil
+	if getMetadataRequest.Plugin == "dataverse" {
+		from := "/api/v1/datasets/:persistentId/versions/:latest?persistentId=" + getMetadataRequest.RepoName
+		fromClient := dv.NewClient(getMetadataRequest.PluginId, getMetadataRequest.Url, user, getMetadataRequest.Token)
+		fromRequest := fromClient.NewRequest(from, "GET", nil, nil)
+		res := map[string]interface{}{}
+		err := api.Do(ctx, fromRequest, &res)
+		if err != nil {
+			return nil, err
+		}
+		if res["status"] != "OK" {
+			return nil, fmt.Errorf("metadata copy failed: %v", res["message"])
+		}
+		metadataBlocks := map[string]interface{}{}
+		metadataBlocks["metadataBlocks"] = res["data"].(map[string]interface{})["metadataBlocks"]
+		return types.Metadata{"datasetVersion": metadataBlocks}, nil
 	}
-	from := "/api/v1/datasets/:persistentId/versions/:latest?persistentId=" + getMetadataRequest.RepoName
-	fromClient := dv.NewClient(getMetadataRequest.PluginId, getMetadataRequest.Url, user, getMetadataRequest.Token)
-	fromRequest := fromClient.NewRequest(from, "GET", nil, nil)
-	res := map[string]interface{}{}
-	err := api.Do(ctx, fromRequest, &res)
+	userObj, err := dataverse.GetUser(ctx, getMetadataRequest.Token, user)
 	if err != nil {
 		return nil, err
 	}
-	if res["status"] != "OK" {
-		return nil, fmt.Errorf("metadata copy failed: %v", res["message"])
+	md := types.MetadataStruct{Author: []types.Author{{AuthorName: fmt.Sprintf("%v, %v", userObj.Data.LastName, userObj.Data.FirstName)}}}
+
+	//TODO
+	// citation.cff --> (2) codemeta.json --> (3) ro-crate.json --> (4) inherente system metadata (via de source API)
+
+	var b bytes.Buffer
+	writer := bufio.NewWriter(&b)
+	err = metadata.Execute(writer, md)
+	if err != nil {
+		return nil, err
 	}
-	metadataBlocks := map[string]interface{}{}
-	metadataBlocks["metadataBlocks"] = res["data"].(map[string]interface{})["metadataBlocks"]
-	return types.Metadata{"datasetVersion": metadataBlocks}, nil
+	writer.Flush()
+	res := types.Metadata{}
+	err = json.Unmarshal(b.Bytes(), &res)
+	if err != nil {
+		return nil, err
+	}
+	return res, nil
 }
+
+var metadata, _ = template.New("metadata").Parse(`
+{
+    "datasetVersion": {
+        "metadataBlocks": {
+            "citation": {
+                "displayName": "Citation Metadata",
+                "name": "citation",
+                "fields": [
+                    {{ if (ne .Title "") }}{
+                        "typeName": "title",
+                        "multiple": false,
+                        "typeClass": "primitive",
+                        "value": "{{ .Title }}"
+                    },{{ end }}{{ if .AlternativeTitle }}{
+                        "typeName": "alternativeTitle",
+                        "multiple": true,
+                        "typeClass": "primitive",
+                        "value": [
+                            {{ range $index, $element := .AlternativeTitle }}{{ if $index }},
+                            {{ end }}"{{ $element }}"{{ end }}
+                        ]
+                    },{{ end }}{{ if .AlternativeURL }}{
+                        "typeName": "alternativeURL",
+                        "multiple": true,
+                        "typeClass": "primitive",
+                        "value": [
+                            {{ range $index, $element := .AlternativeURL }}{{ if $index }},
+                            {{ end }}"{{ $element }}"{{ end }}
+                        ]
+                    },{{ end }}{{ if .OtherId }}{
+                        "typeName": "otherId",
+                        "multiple": true,
+                        "typeClass": "compound",
+                        "value": [
+                            {{ range $index, $element := .OtherId }}{{ if $index }},
+                            {{ end }}{
+                                "otherIdAgency": {
+                                    "typeName": "otherIdAgency",
+                                    "multiple": false,
+                                    "typeClass": "primitive",
+                                    "value": "{{ $element.OtherIdAgency }}"
+                                },
+                                "otherIdValue": {
+                                    "typeName": "otherIdValue",
+                                    "multiple": false,
+                                    "typeClass": "primitive",
+                                    "value": "{{ $element.OtherIdValue }}"
+                                }
+                            }{{ end }}
+                        ]
+                    },{{ end }}{{ if .DsDescription }}{
+                        "typeName": "dsDescription",
+                        "multiple": true,
+                        "typeClass": "compound",
+                        "value": [
+                            {{ range $index, $element := .DsDescription }}{{ if $index }},
+                            {{ end }}{
+                                "dsDescriptionValue": {
+                                    "typeName": "dsDescriptionValue",
+                                    "multiple": false,
+                                    "typeClass": "primitive",
+                                    "value": "{{ $element }}"
+                                }
+                            }{{ end }}
+                        ]
+                    },{{ end }}{{ if .Keyword }}{
+                        "typeName": "keyword",
+                        "multiple": true,
+                        "typeClass": "compound",
+                        "value": [
+                            {{ range $index, $element := .Keyword }}{{ if $index }},
+                            {{ end }}{
+                                "keywordValue": {
+                                    "typeName": "keywordValue",
+                                    "multiple": false,
+                                    "typeClass": "primitive",
+                                    "value": "{{ $element }}"
+                                }
+                            }{{ end }}
+                        ]
+                    },{{ end }}{{ if .ContributorName }}{
+                        "typeName": "contributor",
+                        "multiple": true,
+                        "typeClass": "compound",
+                        "value": [
+                            {{ range $index, $element := .ContributorName }}{{ if $index }},
+                            {{ end }}{
+                                "contributorType": {
+                                    "typeName": "contributorType",
+                                    "multiple": false,
+                                    "typeClass": "controlledVocabulary",
+                                    "value": "Other"
+                                },
+                                "contributorName": {
+                                    "typeName": "contributorName",
+                                    "multiple": false,
+                                    "typeClass": "primitive",
+                                    "value": "{{ $element }}"
+                                }
+                            }{{ end }}
+                        ]
+                    },{{ end }}{{ if .GrantNumber }}{
+                        "typeName": "grantNumber",
+                        "multiple": true,
+                        "typeClass": "compound",
+                        "value": [
+                            {{ range $index, $element := .GrantNumber }}{{ if $index }},
+                            {{ end }}{
+                                "grantNumberAgency": {
+                                    "typeName": "grantNumberAgency",
+                                    "multiple": false,
+                                    "typeClass": "primitive",
+                                    "value": "{{ $element.GrantNumberAgency }}"
+                                },
+                                "grantNumberValue": {
+                                    "typeName": "grantNumberValue",
+                                    "multiple": false,
+                                    "typeClass": "primitive",
+                                    "value": "{{ $element.GrantNumberValue }}"
+                                }
+                            }{{ end }}
+                        ]
+                    },{{ end }}{{ if .RelatedMaterialCitation }}{
+                        "typeName": "relatedMaterial",
+                        "multiple": true,
+                        "typeClass": "compound",
+                        "value": [
+                            {{ range $index, $element := .RelatedMaterialCitation }}{{ if $index }},
+                            {{ end }}{
+                                "relatedMaterialCitation": {
+                                    "typeName": "relatedMaterialCitation",
+                                    "multiple": false,
+                                    "typeClass": "primitive",
+                                    "value": "{{ $element }}"
+                                }
+                            }{{ end }}
+                        ]
+                    },{{ end }}{
+                        "typeName": "author",
+                        "multiple": true,
+                        "typeClass": "compound",
+                        "value": [
+                            {{ range $index, $element := .Author }}{{ if $index }},
+                            {{ end }}{
+                                "authorName": {
+                                    "typeName": "authorName",
+                                    "multiple": false,
+                                    "typeClass": "primitive",
+                                    "value": "{{ $element.AuthorName }}"
+                                }{{ if (ne $element.AuthorAffiliation "") }},
+                                "authorAffiliation": {
+                                    "typeName": "authorAffiliation",
+                                    "multiple": false,
+                                    "typeClass": "primitive",
+                                    "value": "{{ $element.AuthorAffiliation }}"
+                                }{{ end }}{{ if (ne $element.AuthorIdentifier "") }},
+                                "authorIdentifierScheme": {
+                                    "typeName": "authorIdentifierScheme",
+                                    "multiple": false,
+                                    "typeClass": "controlledVocabulary",
+                                    "value": "ORCID"
+                                },
+                                "authorIdentifier": {
+                                    "typeName": "authorIdentifier",
+                                    "multiple": false,
+                                    "typeClass": "primitive",
+                                    "value": "{{ $element.AuthorIdentifier }}"
+                                }{{ end }}
+                            }{{ end }}
+                        ]
+                    }
+                ]
+            }
+        }
+    }
+}
+`)
--- image/app/plugin/types/metadata.go
+++ image/app/plugin/types/metadata.go
@@ -4,6 +4,35 @@ const NewDataset = "New Dataset"
 
 type Metadata map[string]interface{}
 
+type MetadataStruct struct {
+	Title string
+	AlternativeTitle []string
+    AlternativeURL []string
+	OtherId []OtherId
+	Author []Author
+	DsDescription []string
+	Keyword []string
+	ContributorName []string
+    GrantNumber []GrantNumber
+	RelatedMaterialCitation []string
+}
+
+type OtherId struct {
+    OtherIdAgency string
+    OtherIdValue string
+}
+
+type Author struct {
+    AuthorName string
+    AuthorAffiliation string
+    AuthorIdentifier string
+}
+
+type GrantNumber struct {
+    GrantNumberValue string
+    GrantNumberAgency string
+}
+
 type GetMetadataRequest struct {
 	PluginId string `json:"pluginId"`
 	Plugin   string `json:"plugin"`
